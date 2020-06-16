b80ec975eb062aefa368027728e9db4aa168394d
Ryan McCall <ryan.e.mccall@gmail.com>
2020-06-16 16:06:41 -0500

    Add text prediction pages

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 import numpy as np
+from model import predict
 
 app = Flask(__name__)
 
@@ -11,3 +13,15 @@ def index():
 def roll_dice(num_of_dice):
     random_numbers = np.random.randint(1, 6, int(num_of_dice))
     return render_template('roll_dice.html', rolls=random_numbers)
+
+@app.route('/check_text_form')
+def check_text_form():
+    return render_template('check_text_form.html')
+
+@app.route('/check_text_results', methods=['POST'])
+def handle_check_text_form(): 
+    text_content = request.form['text-content']
+
+    text_results = predict(text_content)
+
+    return render_template('check_text_results.html', text_results=text_results)
