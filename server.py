952c1ea8522489ce53ea6e77e6b5ec34dcbad7c5
Ryan McCall <ryan.e.mccall@gmail.com>
2020-06-16 14:48:07 -0500

    Add render templates

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 import numpy as np
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return render_template('index.html')
 
 @app.route('/roll_dice/<num_of_dice>')
 def roll_dice(num_of_dice):
-    random_number = np.random.randint(1, 6, int(num_of_dice))
-    return str(random_number)
+    random_numbers = np.random.randint(1, 6, int(num_of_dice))
+    return render_template('roll_dice.html', rolls=random_numbers)
